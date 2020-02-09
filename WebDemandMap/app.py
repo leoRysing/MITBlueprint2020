6a3ff4a77cc4c0caaa1724eb8611501b564cf7a7
LeviBloch <levi.bloch@gmail.com>
2020-02-09 11:27:24 -0500

    Can display map
    yup

--- WebDemandMap/app.py
+++ WebDemandMap/app.py
@@ -1,1 +1,24 @@
-#yay python
+#!/usr/bin/env python3
+from flask import Flask, render_template, session, request
+#from pymongo import MongoClient
+#leroy is tremendously cute
+app = Flask(__name__)
+
+#messages = []
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html',
+        #message = request.form['message'], 
+        loggedIn=('email' in session),
+        email=session.get('email', '')
+        ) 
+
+        
+@app.route('/register', methods=['POST'])
+def register():
+    session['email'] = request.form['email']
+    print('email went through')
+    return 'success'
+
+app.run(port=3000, debug=True)
--- WebDemandMap/static/main.js
+++ WebDemandMap/static/main.js
@@ -1,1 +1,24 @@
 // stuff
+$(document).ready(function() {
+    $('#register').click(function() {
+        const email = $('#email').val();
+        $.post('/register', {
+            email: email,
+        }).done(function () {
+            document.location.reload();
+        });
+    });
+    $('#logout').click(function() {
+        $.post('/logout', {}).done(function() {
+            document.location.reload();
+        });
+    });
+    $('#submit').click(function () {
+        const message = $('#message').val();
+        $.post('/message', {
+            message: message,
+        }).done(function () {
+            document.location.reload();
+        });
+    });
+});
